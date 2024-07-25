2f6876b0f6ec282d6d51e46797dc45a9d4a485cf
Aniruddha Sg <aniruddhasg@Aniruddhas-MBP.Dlink>
2024-07-25 14:02:24 +0530

    user login

--- app/validators/user-validation-schema.js
+++ app/validators/user-validation-schema.js
@@ -48,4 +48,38 @@ export const userRegisterSchema = {
     }
 }
 
-export const userLoginSchema = {}
+export const userLoginSchema = {
+    email: {
+        exists: { 
+            errorMessage: 'email field is required'
+        }, 
+        notEmpty:{
+            errorMessage: 'email is required'
+        },
+        isEmail: {
+            errorMessage: 'email should be valid format'
+        },
+        trim: true, 
+        normalizeEmail: true 
+    },
+    password: {
+        exists: {
+            errorMessage: 'password field is required'
+        },
+        notEmpty: {
+            errorMessage: 'password cannot be emtpy'
+        }, 
+        isStrongPassword: {
+            options: {
+                minLength: 8,
+                maxLength: 128, 
+                minLowerCase: 1,
+                minUpperCase: 1, 
+                minSymbol: 1,
+                minNumber: 1
+            },
+            errorMessage: 'password should consists of atleast one lower case, one upper case, one symbol, one number and should be atleast 8 characters'
+        },
+        trim: true, 
+    }
+}
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { checkSchema } from 'express-validator'
 dotenv.config() 
 import configureDB from './config/db.js'
 import usersCltr from './app/controllers/users-cltr.js'
-import { userRegisterSchema } from './app/validators/user-validation-schema.js'
+import { userRegisterSchema, userLoginSchema } from './app/validators/user-validation-schema.js'
 const port = process.env.PORT || 3050
 const app = express() 
 configureDB() 
@@ -12,6 +12,13 @@ configureDB()
 app.use(express.json())
 
 app.post('/api/users/register', checkSchema(userRegisterSchema), usersCltr.register)
+app.post('/api/users/login', checkSchema(userLoginSchema), usersCltr.login)
+
+
+// axios.post(url, formData, options)
+// axios.get(url, options)
+// axios.put(url, formData, options)
+// axios.delete(url, options)
 
 app.listen(port, () => {
     console.log('Server running on port', port )
