62b25a95fe8060ebf7c29a8837669ca65d0321e1
Aniruddha Sg <aniruddhasg@Aniruddhas-MBP.Dlink>
2024-07-25 12:27:21 +0530

    users controller - register action

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from 'express' 
 import dotenv from 'dotenv'
+import { checkSchema } from 'express-validator'
 dotenv.config() 
 import configureDB from './config/db.js'
+import usersCltr from './app/controllers/users-cltr.js'
+import { userRegisterSchema } from './app/validators/user-validation-schema.js'
 const port = process.env.PORT || 3050
 const app = express() 
 configureDB() 
+
+app.use(express.json())
+
+app.post('/api/users/register', checkSchema(userRegisterSchema), usersCltr.register)
 
 app.listen(port, () => {
     console.log('Server running on port', port )
